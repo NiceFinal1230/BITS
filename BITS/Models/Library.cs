51e194909b9b8bdc7a70fcdfbf7098f2e2c8e959
Li Yang <chungliyang@yahoo.com.my>
2024-06-30 13:55:25 +0800

    Created Library Page
    + changed the BITS text into the logo
    + created the sorting buttons, but functionality needs work
    + game items in array is hard-coded for now
    + search bar exists, but no functionality
    + created layout buttons, but also no functionality

---
+++ BITS/Models/Library.cs
@@ -0,0 +1,11 @@
+﻿namespace BITS.Models
+{
+    public class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public bool Favorite {  get; set; }
+    }
+}
