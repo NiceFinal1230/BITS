b69c79162a2e4f3c3d89b9d761296561c50f72f2
NiceFinal1230 <lohdiingyang@gmail.com>
2024-07-05 14:44:13 +0800

    Update Source.cs

--- BITS/Models/Source.cs
+++ BITS/Models/Source.cs
@@ -6,7 +6,9 @@ public class Source
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string ExternalLink { get; set; }
-    public string Types { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ExternalLink { get; set; } = string.Empty;
+    // if string.Empty does not exist
+    // when initialize database with seed data, it will crash
+    public string Types { get; set; } = string.Empty;
 }
