a7bb8cc856178c955cb7cea366628b75bf00dfbe
NiceFinal1230 <lohdiingyang@gmail.com>
2024-07-22 01:51:59 +0800

    Complete of Home

--- BITS/Controllers/HomeController.cs
+++ BITS/Controllers/HomeController.cs
@@ -2,21 +2,56 @@ using BITS.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using BITS.Areas.Identity.Data;
+using BITS.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using BITS.ViewModel;
 
 namespace BITS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BITSContext _context;
+        private UserManager<BITSUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BITSContext context, UserManager<BITSUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductStocktakeViewModel> vm = new();
+            HomeViewModel home = new();
+            var list = await _context.Product.ToListAsync();
+            foreach (var p in list)
+            {
+                //var stock = await _context.Stocktake.FirstOrDefaultAsync(s => s.ProductId == p.ProductId && s.Quantity > 0);
+                var stock = await _context.Stocktake.FirstOrDefaultAsync(s => s.ProductId == p.ProductId);
+                if(stock == null)
+                {
+                    stock = new();
+                }
+                vm.Add(new() { Product = p, Stocktake = stock });
+            }
+
+            home.Fixed = vm.TakeLast(5).ToList();
+            vm = vm.SkipLast(5).ToList();
+
+            home.NewRelease = vm.OrderByDescending(p => p.Product.ReleaseDate).Take(5).ToList();
+            vm = vm.Skip(5).ToList();
+
+            home.MegaSale = vm.OrderByDescending(i => i.Stocktake.DiscountRate).Take(5).ToList();
+            vm = vm.Skip(5).ToList();
+
+            home.Explore = vm;
+
+            return View(home);
         }
 
         public IActionResult Privacy()
---
+++ BITS/ViewModel/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BITS.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BITS.ViewModel;
+public class HomeViewModel
+{
+    public List<ProductStocktakeViewModel> Fixed { get; set; } = new List<ProductStocktakeViewModel>();
+    public List<ProductStocktakeViewModel> NewRelease { get; set; } = new List<ProductStocktakeViewModel>();
+    public List<ProductStocktakeViewModel> MegaSale { get; set; } = new List<ProductStocktakeViewModel>();
+    public List<ProductStocktakeViewModel> Explore { get; set; } = new List<ProductStocktakeViewModel>();
+
+}
+
